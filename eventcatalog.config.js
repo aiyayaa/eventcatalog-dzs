f0386d2667d23a064cf3b63ecb98ea97d5dd529a
aiyayaa <1471241769@qq.com>
2026-06-08 15:52:50 +0800

    更新 eventcatalog.config.js 配置

--- eventcatalog.config.js
+++ eventcatalog.config.js
@@ -2,7 +2,7 @@ export default {
   cId: '5b7db671-40d3-4d6a-a8c2-dzs-eventcatalog',
   title: '数据处理与研究支持分系统 EventCatalog',
   tagline: '太空探源专项地面支撑系统数据处理与研究支持分系统接口目录',
-  organizationName: '中国科学院国家空间科学中心',
+  organizationName: '数据处理与研究支持分系统',
 
   base: '/eventcatalog-dzs/',
   output: 'static',
